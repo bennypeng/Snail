0d20d94bd50079f9d92d116dcf67af2909a54d15
bennypeng <303810616@qq.com>
2018-09-08 11:19:16 +0800

    cert vaild

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,22 @@
 //  日志查询
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
+//  证书验证
+Route::get('.well-known/acme-challenge/{file}', function () {
+
+    $arg1 = Route::input('file');
+
+    if (file_exists(storage_path($arg1))) {
+        $content = file_get_contents(storage_path($arg1));
+
+        $response = Response::make($content);
+
+        $response->headers->set('Content-type','text/plain');
+
+        return $response;
+    }
+});
+
 //  测试vue页面
 //Route::group(['middleware' => ['cors']], function() {
 //    Route::get('/web', function() {
